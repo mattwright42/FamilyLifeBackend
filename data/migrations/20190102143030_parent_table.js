96042a381960c8923d5da0b67ae927e6fa264ff6
SavannahGreuel <smallidgesavannah@gmail.com>
2019-01-02 14:42:39 -0700

    added migrations and configured database file

--- data/migrations/20190102143030_parent_table.js
+++ data/migrations/20190102143030_parent_table.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('parents', function(tbl) {
+        // primary key
+        tbl.increments();
+    
+        // other fields
+        tbl
+            .string('name', 255)
+            .notNullable();
+
+        tbl.string('email', 128);
+    
+        
+      });
+    };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('parents');
 };
