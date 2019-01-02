df0a06d9186201bf757971d788ba2506219c1923
Matthew Wright <matthewbwright@gmail.com>
2019-01-02 13:21:47 -0700

    adds server, data, and index files

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const db = require('../data/dbConfig.js');
+const morgan = require('morgan');
+
+const port = process.env.PORT || 9000;
+
+server.use(express.json());
+server.use(cors());
+server.use(morgan('dev'));
+
+//Sanity Check
+server.get('/', (req, res) => {
+  res.send(`API running on port: ${port}`);
+});
